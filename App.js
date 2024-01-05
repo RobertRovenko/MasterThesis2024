cbd881400d2bcfadeb51b621ce36f620a1d994cc
Robert Rovenko <robert.ntisthlm@gmail.com>
2024-01-05 22:20:28 +0100

    Created an Expo app, set up navigation and connected to github

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import GameScreen from './screens/GameScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} 
+         options={{
+          title: null, // Set a custom title for the screen
+          headerLeft: null, // Hide the back button
+        }}/>
+        <Stack.Screen name="GameScreen" component={GameScreen} 
+         options={{
+          title: null, // Set a custom title for the screen
+           // Hide the back button
+        }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
