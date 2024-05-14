f1fc266287e24732c497be6ae1daa8eda4739682
Robert Rovenko <robert.ntisthlm@gmail.com>
2024-05-14 11:36:17 +0200

    succesfully made a build for AAB to publish my app

--- screens/LandingPage.js
+++ screens/LandingPage.js
@@ -78,7 +78,7 @@ const LandingPage = ({ navigation }) => {
   return (
     <ImageBackground
       style={styles.background}
-      source={require("../img/landingimage2.jpg")}
+      source={require("../assets/sscreen.png")}
     >
       <View style={styles.container}>
         <Text style={styles.textLogo}>30 Day{"\n"}Fitness</Text>
